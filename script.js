da7d7f67586d8e1b99dab00318f5c110c9f3348a
morganfula <morgan.fula@me.com>
2021-01-17 11:45:21 +0100

    done

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const character = document.getElementById('player');
+const object = document.getElementById('object');
+
+console.log(object);
 
 function jump() {
   if (character.classList != 'jump') {
@@ -9,6 +12,24 @@ function jump() {
     }, 300);
   }
 }
+
+let isAlive = setInterval(() => {
+  // get current character Y position
+  let playerTop = parseInt(
+    window.getComputedStyle(character).getPropertyValue('top')
+  );
+
+  // get current object X position
+  let objectLeft = parseInt(
+    window.getComputedStyle(object).getPropertyValue('left')
+  );
+
+  // detect position
+  if (objectLeft < 50 && objectLeft > 0 && playerTop >= 140) {
+    // collision
+    alert('Game Over!');
+  }
+}, 10);
 
 document.addEventListener('keydown', function (event) {
   jump();
